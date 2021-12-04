4cff86c99078f667b2cb916b38a1c5fe715edebe
yghg <git@yoanhermida.com>
2021-12-04 09:29:45 -0500

    GUI changes.

--- gui.py
+++ gui.py
@@ -1,16 +1,16 @@
-from tkinter import Tk, Label, Button, LEFT
+from tkinter import Tk, Label, Button, LEFT, Entry, END
 
 from data import MyData, DatabaseClient
 
 
 class UserInterface(object):
-
+    
     client = DatabaseClient("fireapp.db")
 
     # creates root window
-    root = Tk(className="FIRE")
+    root = Tk(className="fireApp")
     root.geometry("500x500")
-    root.title("FIRE Monthly Calculator")
+    root.title("FIRE App")
 
     # window components
     welcome = Label(root, text='Net Worth\n')
@@ -19,8 +19,18 @@ class UserInterface(object):
     def exit_app(self):
         self.root.destroy()
 
+    def display_records(self):
+        r_set = MyData(self.client).get_record()
+        i = 0
+        for r in r_set:
+            for j in range(len(r)):
+                e = Entry(self.root, width=10, fg='blue')
+                e.grid(row=i, column=j)
+                e.insert(END, r[j])
+            i += 1
+
     # buttons
-    button_get_doc = Button(root, text="Get Net Worth", command=MyData(client).get_record())
+    button_get_doc = Button(root, text="Get Net Worth", command=display_records)
     button_get_doc.pack(side=LEFT)
 
     button_exit = Button(root, text="Exit", command=exit_app)
